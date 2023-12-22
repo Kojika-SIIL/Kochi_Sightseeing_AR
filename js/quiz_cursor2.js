1bb7d38151b6b92e84c8ed716e20f3746407f07f
こじか <130383143+Kojika-SIIL@users.noreply.github.com>
2023-12-22 12:28:27 +0900

    Update quiz_cursor2.js

--- js/quiz_cursor2.js
+++ js/quiz_cursor2.js
@@ -77,9 +77,7 @@ function changeColorOnHover(element) {
     originalColor  = element.getAttribute('material').color;
     if (cursorEnabled) {
       element.setAttribute('material', 'color: #f0e68c');
-      element.addEventListener('click', function() {
-        tapButton(element);
-      }
+      tapButton(element);
     }
   });
 
@@ -94,18 +92,20 @@ function changeColorOnHover(element) {
 /*----- ボタンをタップしたときの処理 -----*/
 function tapButton(element) {
   var button = document.getElementById('chooseAnswerButton');
-  // 背景色を薄くする処理
-  button.style.backgroundColor = 'rgba(135, 250, 0, 1)';
-  setTimeout(function() {
-    // 一定時間後に背景色を元に戻す処理
-    button.style.backgroundColor = 'rgba(0, 0, 255, 1)';
-  }, 300); // 300ミリ秒後に元に戻す（必要に応じて調整）
+  button.addEventListener('click', function () {
+    // 背景色を薄くする処理
+    button.style.backgroundColor = 'rgba(135, 250, 0, 1)';
+    setTimeout(function() {
+      // 一定時間後に背景色を元に戻す処理
+      button.style.backgroundColor = 'rgba(0, 0, 255, 1)';
+    }, 300); // 300ミリ秒後に元に戻す（必要に応じて調整）
 
-  if (element === correct01 || element === correct02 || element === correct03) {
-    correctOnHover(element);
-  } else {
-    incorrectOnHover(element);
-  }
+    if (element === correct01 || element === correct02 || element === correct03) {
+      correctOnHover(element);
+    } else {
+      incorrectOnHover(element);
+    }
+  });
 }
       
 /*----- 正解・不正解の処理 -----*/
